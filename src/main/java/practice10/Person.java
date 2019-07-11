c7f6f6babb6abe9e6f1ce4580aa9f856ad3da61c
Tiger Zhu <tiger.zhu@oocl.com>
2019-07-12 01:09:06 +0800

    finish the test of practice10

--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -1,4 +1,40 @@
 package practice10;
 
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Person) {
+            Person person = (Person) obj;
+            return id == person.id;
+        }
+        return false;
+    }
+
+
+    public Person(){
+
+    }
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        return "My name is "+this.name+". I am "+this.age+" years old.";
+    }
 }
