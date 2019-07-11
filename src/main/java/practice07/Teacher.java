ff1c36f59df27925488c53bdc45f738f72219f5b
Tiger Zhu <tiger.zhu@oocl.com>
2019-07-11 21:47:47 +0800

    finish the test of practice07

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,37 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    public Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+    public String introduce(){
+        String outPut;
+        if(this.klass ==null || this.klass.getNumber()==0)
+        {
+            outPut=" I am a Teacher. I teach No Class.";
+        }else {
+            outPut=" I am a Teacher. I teach "+this.klass.getDisplayName()+".";
+        }
+        return super.introduce()+outPut;
+    }
+    public String introduceWith(Student student){
+        String outPut;
+        if(student.getKlass().getNumber()==this.klass.getNumber()){
+            outPut=" I am a Teacher. I teach "+student.getName()+".";
+        }else {
+            outPut=" I am a Teacher. I don't teach "+student.getName()+".";
+        }
+        return super.introduce()+outPut;
+    }
 }
