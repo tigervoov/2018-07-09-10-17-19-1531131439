6c27be08f0ca9b162a485a702a8c83794272895c
Tiger Zhu <tiger.zhu@oocl.com>
2019-07-11 20:54:57 +0800

    finish the test of practice04

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,12 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduce(){
+
+        return "My name is "+super.getName()+". I am "+super.getAge()+" years old. "+"I am a Worker. I have a job.";
+    }
 }
