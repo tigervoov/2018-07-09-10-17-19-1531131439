ed3cf87b79ee7d7d82dafee12d159b05eaf2a956
Tiger Zhu <tiger.zhu@oocl.com>
2019-07-11 21:21:36 +0800

    finish the test of practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    public int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        String outPut;
+        if(this.klass==0)
+        {
+            outPut=" I am a Teacher. I teach No Class.";
+        }else {
+            outPut=" I am a Teacher. I teach Class"+this.klass+".";
+        }
+        return super.introduce()+outPut;
+    }
 }
